e4156051af04dbb94f2ee1c735e9b83b6f55957a
ic3evi1 <270490285@qq.com>
2019-11-07 15:27:13 +0800

    主程序循环，用户输入0时候退出

--- cards_main.py
+++ cards_main.py
@@ -1,19 +1,20 @@
 #TODO(ic3evi1) 名片管理系统【练习版】
 
+#主程序无限循环，用户输入0时，执行退出
+while True:
+    action_str = input("请选择操作功能：")
+    print("您选择的功能是： %s " % action_str)
 
-action_str = input("请选择操作功能：")
-print("您选择的功能是： %s " % action_str)
+    #针对 1、2、3输入的操作功能
+    if action_str in ["1","2","3"]:
+        #代码未完成，用pass站位保证程序能够运行
+        pass
 
-#针对 1、2、3输入的操作功能
-if action_str in ["1","2","3"]:
-    #代码未完成，用pass站位保证程序能够运行
-    pass
+    #用户输入0，退出程序
+    elif action_str == "0":
+        print("欢迎下次使用[名片管理系统]！")
+        break
 
-#用户输入0，退出程序
-elif action_str == "0":
-    print("欢迎下次使用[名片管理系统]！")
-    pass
-
-#其他用户输入，提示输入错误
-else:
-    print("输入错误，请继续输入...")
+    #其他用户输入，提示输入错误
+    else:
+        print("输入错误，请继续输入...")
